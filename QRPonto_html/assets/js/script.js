af8dd940f7ceab830f7f84b02637dc9c3d0cad7a
Marcos Carvalho de Assis <mcarvalhodash@gmail.com>
2017-06-10 15:30:32 -0300

    Changes to be committed: 	modified:   QRPonto_html/assets/js/script.js 	modified:   QRPonto_html/index.html

--- QRPonto_html/assets/js/script.js
+++ QRPonto_html/assets/js/script.js
@@ -1,5 +1,10 @@
-var decode = function() {
-	$('#file').files
+var decode = function () {
+    var reader = new FileReader();
+    // read the image file as a data URL.
+    var dataUrl = reader.readAsDataURL($('#file')[0].files[0]);
+
+    $('#uploaded')[0].src = dataUrl;
 
 
-}
+
+};
