85fd6d1e824c047cf7a3964c0d71c8b7d9614ee1
Marcos Carvalho de Assis <mcarvalhodash@gmail.com>
2017-06-10 17:18:21 -0300

    Input de camera

--- QRPonto_html/assets/js/script.js
+++ QRPonto_html/assets/js/script.js
@@ -1,9 +1,15 @@
 var decode = function () {
     var reader = new FileReader();
     // read the image file as a data URL.
-    var dataUrl = reader.readAsDataURL($('#file')[0].files[0]);
+    qrcode.callback = function(decoded) { alert(decoded) };
+    reader.onload = function (e) {
+        // get loaded data and render thumbnail.
+	    $('#uploaded')[0].src = e.target.result;
+		qrcode.decode(e.target.result);
+    };
 
-    $('#uploaded')[0].src = dataUrl;
+    // read the image file as a data URL.
+    reader.readAsDataURL($('#cameraInput')[0].files[0]);
 
 
 
